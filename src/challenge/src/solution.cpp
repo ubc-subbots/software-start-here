736ebab1c5b662a021d857a0fd6c3697d6b2fec9
Fei Kuan <fei_fei_0316@hotmail.com>
2022-06-04 15:15:20 -0700

    DONE!!!

---
+++ src/challenge/src/solution.cpp
@@ -0,0 +1,81 @@
+// Copyright 2016 Open Source Robotics Foundation, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <chrono>
+#include <functional>
+#include <memory>
+#include <string>
+#include <math.h>
+
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include <geometry_msgs/msg/twist.hpp>
+
+using namespace std::chrono_literals;
+
+/* This example creates a subclass of Node and uses std::bind() to register a
+ * member function as a callback from the timer. */
+
+class MinimalPublisher : public rclcpp::Node
+{
+public:
+  MinimalPublisher()
+      : Node("minimal_publisher"), count_(0.0)
+  {
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("/turtle1/cmd_vel", 10);
+    timer_ = this->create_wall_timer(
+        100ms, std::bind(&MinimalPublisher::timer_callback, this));
+  }
+
+private:
+  void timer_callback()
+  {
+    auto message = geometry_msgs::msg::Twist();
+    //    message.data = "Hello, world! " + std::to_string(count_++);
+    //    RCLCPP_INFO(this->get_logger(), "Publishing: '%s'", message.data.c_str());
+    // if (count_++ % 2)
+    // {
+      // count_ = count_ + 3.141592653589/100.0;
+      count_ += 3.14159/10;
+      message.linear.x = 1.0;
+      message.linear.y = 0.0;
+      message.linear.z = 0.0;
+      message.angular.x = 0.0;
+      message.angular.y = 0.0;
+      message.angular.z = 1.8*sin(count_);
+      publisher_->publish(message);
+    // }
+    // else
+    // {
+    //   message.linear.x = 0.0;
+    //   message.linear.y = 0.0;
+    //   message.linear.z = 0.0;
+    //   message.angular.x = 0.0;
+    //   message.angular.y = 0.0;
+    //   message.angular.z = 0.0;
+    //   publisher_->publish(message);
+    // }
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  double_t count_;
+};
+
+int main(int argc, char *argv[])
+{
+  rclcpp::init(argc, argv);
+  rclcpp::spin(std::make_shared<MinimalPublisher>());
+  rclcpp::shutdown();
+  return 0;
+}
