0d3da23d161977f5b360944733a20e2eb6aae231
navrajb <navrajb03@gmail.com>
2022-11-19 12:23:36 -0800

    launch file

---
+++ src/challenge/launch/sinusoidal_Launcher.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='turtlesim',
+            executable='turtlesim_node',
+            name='turtle1'
+        ),
+        Node(
+            package='challenge',
+            executable='solution',
+            name='Sine_wave',
+
+        )
+      
+    ])
