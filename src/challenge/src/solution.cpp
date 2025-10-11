0200505b4668bbb2be71758a15cb1b81ea1e01fe
Griffin0-0 <griffin.ng.butchko@gmail.com>
2025-10-11 13:53:19 -0700

    initial commit - turtle sim with sine wave

--- src/challenge/src/solution.cpp
+++ src/challenge/src/solution.cpp
@@ -1,47 +1,51 @@
 #include <chrono>
+#include <time.h>
+#include <math.h>
 
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/Twist.hpp"
-
+#include "geometry_msgs/msg/twist.hpp"
+#include "turtlesim/msg/pose.hpp"
 
 using namespace std::chrono_literals;
 
-/* This example creates a subclass of Node and uses std::bind() to register a
-* member function as a callback from the timer. */
+class MinimalPublisher : public rclcpp::Node {
 
-class MinimalPublisher : public rclcpp::Node
-{
-  public:
-    MinimalPublisher()
-    : Node("minimal_publisher")
-    {
-      publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
+public:
+  MinimalPublisher()
+      : Node("solution"), count_(0)
+  {
+    publisher_ = this->create_publisher<geometry_msgs::msg::Twist>("turtle1/cmd_vel", 10);
 
-      /* Setting up timer */
-      timer_ = this->create_wall_timer(
-      500ms, std::bind(&MinimalPublisher::timer_callback, this));
+    /* Setting up timer */
+    timer_ = this->create_wall_timer(
+        100ms, std::bind(&MinimalPublisher::timer_callback, this));
+
+  }
+
+private:
+
+  int count_;
+
+  void timer_callback() {
+    
+    if (count_ < 50) {
+
+      auto velocity = geometry_msgs::msg::Twist();
+
+      velocity.linear.x = (0.5);
+      velocity.linear.y = (sin(count_));
+
+      publisher_->publish(velocity);
+      count_++;
+
     }
 
-  private:
-    void timer_callback()
-    /* Where data actually gets published */
-    {
-        auto position = geometry_msgs::msg::Twist();
-        auto start = std::chrono::system_clock::now();
-
-        // position.linear.x = std::chrono::duration<double>(start - std::chrono::system_clock::now());
-        // position.linear.y = std::chrono::duration<double>(1);
-
-        position.linear.x = 1;
-        position.linear.y = 1;
-
-        publisher_->publish(position);
-    }
-    rclcpp::TimerBase::SharedPtr timer_;
-    rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
+  }
+  rclcpp::TimerBase::SharedPtr timer_;
+  rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr publisher_;
 };
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
   rclcpp::init(argc, argv);
   rclcpp::spin(std::make_shared<MinimalPublisher>());
